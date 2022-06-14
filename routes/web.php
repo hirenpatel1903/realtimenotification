5383f67ce65eb18afc18db836e39360170591124
Hiren Patel <patelhiren.hp19@gmail.com>
2022-06-15 00:25:20 +0530

    redis add

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/t', function () {
+    event(new \App\Events\SendMessage());
+    dd('Event Run Successfully.');
+});
+
